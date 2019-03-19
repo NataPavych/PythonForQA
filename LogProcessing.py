92e6460e52ed729a081c28cacbb9d7b050d83c86
NataPavych <npavych@gmail.com>
2019-03-19 19:51:22 +0200

    Update LogProcessing.py

--- LogProcessing.py
+++ LogProcessing.py
@@ -6,40 +6,47 @@ Created on Fri Mar 15 17:07:04 2019
 """
 
 
-def load_file_into_list_by_line(file_name: str) -> object:
+def load_file_into_list_by_line(file_name: str) -> list:
     with open(file_name) as text_file:
         return [line for line in text_file]
 
 
-def clean_data_into_d2_list(list_name):
-    return list(map(lambda string: (lambda x, y, z: [f'{x} {y[1:12]}', int(z)])(*string.split(' ')), list_name))
+def clean_data(list_name):
+    processed = []
+    for string in list_name:
+        str1, str2, str3 = string.split(' ')
+        processed.append([f'{str1} {str2[1:12]}', int(str3)])
+    return processed
 
 
 def get_hours_from(list_name):
-    return list(map(lambda string: string.split(':')[1], list_name))
+    hour_list = []
+    for string in list_name:
+        hour_list.append(string.split(':')[1])
+    return hour_list
 
 
 def sum_values_by_key_into_dictionary(list_name):
     dictionary = {}
     for element in list_name:
         if element[0] in dictionary:
-            dictionary.update({element[0]: dictionary[element[0]]+element[1]})
+            dictionary[element[0]] = dictionary[element[0]]+element[1]
         else:
-            dictionary.update({element[0]: element[1]})
+            dictionary[element[0]] = element[1]
     return dictionary
 
 
 def max_value_by_partial_key(income_dictionary):
     outcome_dictionary = {}
     for key, value in income_dictionary.items():
-        [key1, key2, value] = *key.split(' '), value
+        key1, key2 = key.split(' ')
         if key2 in outcome_dictionary:
             if outcome_dictionary[key2][1] < value:
-                outcome_dictionary.update({key2: (key1, value)})
+                outcome_dictionary[key2] = (key1, value)
             else:
-                outcome_dictionary.update({key2: outcome_dictionary[key2]})
+                outcome_dictionary[key2] = outcome_dictionary[key2]
         else:
-            outcome_dictionary.update({key2: (key1, value)})
+            outcome_dictionary[key2] = (key1, value)
     return outcome_dictionary
 
 
@@ -47,9 +54,9 @@ def count_duplicates_into_dictionary(list_name):
     dictionary = {}
     for element in list_name:
         if element in dictionary:
-            dictionary.update({element: dictionary[element]+1})
+            dictionary[element] = dictionary[element]+1
         else:
-            dictionary.update({element: 1})
+            dictionary[element] = 1
     return dictionary
                
 
@@ -65,7 +72,7 @@ def print_dictionary_partial_value(dictionary_name, n):
 
 # Load and clean data
 raw_data = load_file_into_list_by_line("log_data/log_data.txt")
-cleaned_data = clean_data_into_d2_list(raw_data)
+cleaned_data = clean_data(raw_data)
 
 # Find the top downloader for each date
 sum_by_ip_date = sum_values_by_key_into_dictionary(cleaned_data)
